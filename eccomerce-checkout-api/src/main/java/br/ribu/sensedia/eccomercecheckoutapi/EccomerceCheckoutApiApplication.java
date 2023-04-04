4534b76cc78f36b80f2849255da35c093f224c3e
pauloribu1 <69600205+pauloribu1@users.noreply.github.com>
2023-04-04 16:11:18 -0300

    Add files via upload

---
+++ eccomerce-checkout-api/src/main/java/br/ribu/sensedia/eccomercecheckoutapi/EccomerceCheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package br.ribu.sensedia.eccomercecheckoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceCheckoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceCheckoutApiApplication.class, args);
+	}
+
+}
